e2a0c75fb6835c21223b7d6609c52634dfb3dc9f
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-08-20 16:22:40 +0400

    more cleanup
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -22,3 +22,21 @@ EOF
 
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 dpkg-reconfigure grub-pc
+
+cat <<EOF > /etc/resolv.conf
+nameserver 2001:4860:4860::8888
+nameserver 2001:4860:4860::8844
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+options timeout:2 attempts:1 rotate
+EOF
+
+cat <<EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet6 auto
+iface eth0 inet dhcp
+dns-nameservers 2001:4860:4860::8888 2001:4860:4860::8844 8.8.8.8 8.8.4.4
+EOF
