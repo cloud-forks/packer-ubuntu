5125327be71c114c8061382bfa907d00834671fe
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-02-04 16:33:27 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/cloudinit.sh
+++ scripts/cloudinit.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+apt-get update
 apt-get install -y software-properties-common python-jsonpatch python-prettytable python-setuptools python-requests
 dpkg -i /tmp/cloud-init_0.7.5-0ubuntu1_all.deb
 rm -f /tmp/cloud-init_0.7.5-0ubuntu1_all.deb
